443e0af12ea5cc7902d944293b6de92835d76fc7
bualla450834969-hub <bualla450834969@gmail.com>
2026-09-23 21:04:27 +0800

    v8: 书法行业数据模块 audience.js

---
+++ v8/industries/shufa/data/audience.js
@@ -0,0 +1,54 @@
+// 数据模块：audience
+window.DASHBOARD_DATA_audience = {
+  "audience_personas": [
+    {
+      "name": "书法初学者",
+      "desc": "想练字入门的新手",
+      "keywords": [
+        "入门",
+        "基础",
+        "教程"
+      ],
+      "avgLikes": 5000,
+      "platform": "douyin",
+      "count": 120,
+      "ratio": 35,
+      "content_pref": "教程/入门",
+      "active_time": "晚上8-10点",
+      "monetization": "卖字帖/课程",
+      "pain": "不知道从何开始"
+    },
+    {
+      "name": "硬笔爱好者",
+      "desc": "日常书写爱好者",
+      "keywords": [
+        "硬笔",
+        "钢笔"
+      ],
+      "avgLikes": 8000,
+      "platform": "douyin",
+      "count": 150,
+      "ratio": 44,
+      "content_pref": "手写/实用技巧",
+      "active_time": "中午12-1点",
+      "monetization": "卖钢笔/文具",
+      "pain": "想提升书写速度"
+    },
+    {
+      "name": "书法老师",
+      "desc": "教书法的从业者",
+      "keywords": [
+        "教学",
+        "老师"
+      ],
+      "avgLikes": 3000,
+      "platform": "douyin",
+      "count": 70,
+      "ratio": 21,
+      "content_pref": "教学/干货",
+      "active_time": "上午9-11点",
+      "monetization": "卖课程/服务",
+      "pain": "招生难"
+    }
+  ]
+};
